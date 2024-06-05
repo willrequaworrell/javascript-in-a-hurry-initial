0a5948e4de7e58b3b331424bed483a237bea84e3
Will <willrequaworrell@gmail.com>
2024-06-04 23:49:32 -0400

    finished through video 25

--- main.js
+++ main.js
@@ -12,11 +12,32 @@ document.querySelector("button#close-nav-menu").addEventListener("click", () => 
 
 // Greeting Section 
 
+const convertCelsiusToFahrenheit = (temp) => {
+    return (temp * (9/5)) + 32
+}
+
+const convertFahrenheitToCelsius = (temp) => {
+    return (temp - 32) * (5/9)
+}
+
 const greetingText = "Good Morning"
 const weatherCondition = "sunny"
 const userLocation = "New York"
-let temp = 22.8673
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temp.toFixed(1).toString()} degrees outside`
+let tempInCelsius = 25
+let temp = tempInCelsius
+let weatherTextCelsius = `The weather is ${weatherCondition} in ${userLocation} and it's ${temp.toFixed(1).toString()} degrees outside`
+let weatherTextFahrenheit = `The weather is ${weatherCondition} in ${userLocation} and it's ${convertCelsiusToFahrenheit(temp).toFixed(1).toString()} degrees outside`
 
 document.querySelector("#greeting").innerHTML = greetingText
-document.querySelector("p#weather").innerHTML = weatherText
+document.querySelector("p#weather").innerHTML = weatherTextCelsius
+
+
+// Temperature Toggle Section 
+document.querySelector(".weather-group").addEventListener("click", (e) => {
+    const clickedID = e.target.id
+    if (clickedID === "fahr") {
+        document.querySelector("p#weather").innerHTML = weatherTextFahrenheit
+    } else if (clickedID === "celsius") {
+        document.querySelector("p#weather").innerHTML = weatherTextCelsius
+    }
+})
